a54d491a46f25f790df030ad5c337f05b3edb486
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2017-01-18 00:42:34 -0600

    Interfaz para la radio
    Interfaz a ser utilizada por la clase radio

---
+++ src/intrad.java
@@ -0,0 +1,11 @@
+// Rodrigo Alvarado
+// Carlos Arroyave
+// Samantha Duarte
+
+public interface intrad {
+	public void Estado();
+	public void Frecuencia();
+	public void Guardar(int posicion);
+	public float Seleccionar(int posicion);
+	public void Cambiar (boolean cambio);
+}
