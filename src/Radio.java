9f9d80ec97b9831f8ff6706eed4fb78e4cb7a08c
arr16774 <caarroyavepadilla@gmail.com>
2017-01-17 19:00:29 -0600

    clase y constructor

--- src/Radio.java
+++ src/Radio.java
@@ -1,8 +1,29 @@
 
-public class Radio {
+public class Radio implements Interfaz  {
+	
+		private boolean estado;
+		private boolean frecuencia;
+		private float emisoraFM;
+		private float emisoraAM;
+		private float estacionesGuardadasFM[];
+		private float estacionesGuardadasAM[];
 
 	public Radio() {
-		// TODO Auto-generated constructor stub
+		estado = true;
+		frecuencia = true;
+		emisoraFM = 88.2f;
+		emisoraAM = 510;
+		estacionesGuardadasFM = new float [12];
+		estacionesGuardadasAM = new float [12];
 	}
+	public void  Estado(){
+		if (estado == true){
+			estado = false;
+		}
+		else estado = true;
+		
+	}
+	
+	public void Seleccionar()
 
 }
